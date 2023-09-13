0899f040e4dff2ea0ad6423c4d980531472f8ac7
Moloy <bleads2@gmail.com>
2023-09-12 23:17:42 -0700

    GetSnapshotBeforeUpdate

--- src/life-cycle-methods/getsnapshotbeforeupdate.js
+++ src/life-cycle-methods/getsnapshotbeforeupdate.js
@@ -1,44 +1,39 @@
-import React from 'react';
+import React from "react";
 class GetSnapshotBeforeUpdate extends React.Component {
+  // Initializing the state
+  constructor() {
+    super();
+    this.state = {
+      name: "GFG",
+    };
+  }
+  componentDidMount() {
+    // Changing the state after 1 sec
+    setTimeout(() => {
+      this.setState({ name: "GeeksForGeeks" });
+    }, 2000);
+  }
 
-// Initializing the state
-constructor(){
-  super()
-  this.state = {
-    name: 'GFG',
-  };
-  
-}
-componentDidMount() {
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    // Displaying the previous value of the state
+    document.getElementById("prev").innerHTML =
+      "Previous Name: " + prevState.name;
+  }
 
-	// Changing the state after 1 sec
-	setTimeout(() => {
-	this.setState({ name: 'GeeksForGeeks' });
-	}, 2000);
-}
+  componentDidUpdate() {
+    // Displaying the current value of the state
+    document.getElementById("new").innerHTML =
+      "Current Name: " + this.state.name;
+  }
 
-getSnapshotBeforeUpdate(prevProps, prevState) {
-
-	// Displaying the previous value of the state
-	document.getElementById('prev').innerHTML =
-	'Previous Name: ' + prevState.name;
-}
-
-componentDidUpdate() {
-
-	// Displaying the current value of the state
-	document.getElementById('new').innerHTML =
-	'Current Name: ' + this.state.name;
-}
-
-render() {
-	return (
-	<div>
-		<div id="prev"></div>
-		<div id="new"></div>
-	</div>
-	);
-}
+  render() {
+    return (
+      <div>
+        <div id="prev"></div>
+        <div id="new"></div>
+      </div>
+    );
+  }
 }
 
 export default GetSnapshotBeforeUpdate;

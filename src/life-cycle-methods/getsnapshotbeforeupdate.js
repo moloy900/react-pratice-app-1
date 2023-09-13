1d4464bfd0b9404332d66ff6d8411f910fcf5998
Moloy <bleads2@gmail.com>
2023-09-12 23:17:08 -0700

    Get-Snapshot-Before-Update

--- src/life-cycle-methods/getsnapshotbeforeupdate.js
+++ src/life-cycle-methods/getsnapshotbeforeupdate.js
@@ -1,78 +1,44 @@
-import React, { Component } from "react";
+import React from 'react';
+class GetSnapshotBeforeUpdate extends React.Component {
 
-class GetSnapshotBeforeUpdate extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      items: [],
-      newMessage: "",
-    };
-    this.listRef = React.createRef();
-  }
-
-  componentDidMount() {
-    // Simulate adding items to the list over time.
-    this.interval = setInterval(() => {
-      const { items } = this.state;
-      this.setState({
-        items: [...items, `Item ${items.length + 1}`],
-      });
-    }, 2000);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
-
-  getSnapshotBeforeUpdate(prevProps, prevState) {
-    // Check if new items were added to the list.
-    if (prevState.items.length < this.state.items.length) {
-      // Capture the current scroll position before the update.
-      const list = this.listRef.current;
-      return list.scrollHeight - list.scrollTop;
-    }
-    return null;
-  }
-
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    // If there was a snapshot (scroll position), adjust it after the update.
-    if (snapshot !== null) {
-      const list = this.listRef.current;
-      list.scrollTop = list.scrollHeight - snapshot;
-    }
-  }
-
-  handleInputChange = (e) => {
-    this.setState({ newMessage: e.target.value });
+// Initializing the state
+constructor(){
+  super()
+  this.state = {
+    name: 'GFG',
   };
+  
+}
+componentDidMount() {
 
-  render() {
-    const { items, newMessage } = this.state;
+	// Changing the state after 1 sec
+	setTimeout(() => {
+	this.setState({ name: 'GeeksForGeeks' });
+	}, 2000);
+}
 
-    return (
-      <div>
-        <h1>Scrollable List</h1>
-        <div
-          ref={this.listRef}
-          style={{
-            height: "200px",
-            overflowY: "auto",
-            border: "1px solid #ccc",
-          }}
-        >
-          {items.map((item, index) => (
-            <div key={index}>{item}</div>
-          ))}
-        </div>
-        <input
-          type="text"
-          placeholder="Enter a new item"
-          value={newMessage}
-          onChange={this.handleInputChange}
-        />
-      </div>
-    );
-  }
+getSnapshotBeforeUpdate(prevProps, prevState) {
+
+	// Displaying the previous value of the state
+	document.getElementById('prev').innerHTML =
+	'Previous Name: ' + prevState.name;
+}
+
+componentDidUpdate() {
+
+	// Displaying the current value of the state
+	document.getElementById('new').innerHTML =
+	'Current Name: ' + this.state.name;
+}
+
+render() {
+	return (
+	<div>
+		<div id="prev"></div>
+		<div id="new"></div>
+	</div>
+	);
+}
 }
 
 export default GetSnapshotBeforeUpdate;
